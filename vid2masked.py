632c2dabc79776fe50e4df1a01009c20b05a425a
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2024-07-14 23:34:24 +0900

    updated

--- vid2masked.py
+++ vid2masked.py
@@ -3,10 +3,18 @@ import argparse
 import cv2
 import numpy as np
 from model.humanparsing.run_parsing import Parsing
+from model.openpose.run_openpose import OpenPose
+
 from util.multithread_video_loader import MultithreadVideoLoader
 from util.image2video import Image2VideoWriter
 from tqdm import tqdm
 from PIL import Image
+from util.utils_ootd import get_mask_location
+
+category_dict = ['upperbody', 'lowerbody', 'dress']
+category_dict_utils = ['upper_body', 'lower_body', 'dresses']
+
+model_type = "dc" # "hd" or "dc" dc->full body
 
 
 
@@ -14,21 +22,23 @@ def main(input_video_path="./input_video.mp4", output_video_path="./output_video
     video_loader = MultithreadVideoLoader(input_video_path)
     video_writer = Image2VideoWriter()
     parsing_model = Parsing(gpu_id=0)
+    openpose_model = OpenPose(gpu_id=0)
+
     for i in tqdm(range(len(video_loader))):
         frame = video_loader.cap()
         frame = frame[:, :, [2, 1, 0]]
         frame = Image.fromarray(frame)
-        img_reshaper = ImageReshaper(frame)
-        frame_43 = img_reshaper.get_reshaped()
-        model_img = frame_43.resize((768, 1024))
+        model_img = frame.resize((768, 1024))
         model_parse, _ = parsing_model(model_img.resize((384, 512)))
+        keypoints = openpose_model(model_img.resize((384, 512)))
 
-        mask, mask_gray = get_mask_location(model_type, category_dict_utils[category], model_parse, keypoints)
+        mask, mask_gray = get_mask_location(model_type, 'upper_body', model_parse, keypoints)
         mask = mask.resize((768, 1024), Image.NEAREST)
         mask_gray = mask_gray.resize((768, 1024), Image.NEAREST)
 
         masked_vton_img = Image.composite(mask_gray, model_img, mask)
-        out_frame = densepose_extractor.get_dp_map(frame,isRGB=False)
+        out_frame = np.array(masked_vton_img)[:,:,[2,1,0]]
+
         video_writer.append(out_frame)
     video_writer.make_video(outvid=output_video_path,fps=video_loader.fps_list[0])
 
