a5cbe18eb6a87c4dc60efe17fa60586bf3666e85
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2024-07-14 23:56:54 +0900

    updated

--- download_ckpt.sh
+++ download_ckpt.sh
@@ -3,3 +3,4 @@ cd checkpoints
 mkdir -p humanparsing
 cd humanparsing
 wget https://huggingface.co/levihsu/OOTDiffusion/resolve/main/checkpoints/humanparsing/parsing_atr.onnx
+wget https://huggingface.co/levihsu/OOTDiffusion/resolve/main/checkpoints/humanparsing/parsing_lip.onnx
