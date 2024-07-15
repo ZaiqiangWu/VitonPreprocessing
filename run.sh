548d8cb9cfb52b168b3ae1b9a41973b8dd1caf3a
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2024-07-15 22:44:59 +0900

    updated

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python preprocess.py --input_video ichao.mp4 --name ichao --target ./example
+#python preprocess.py --input_video ichao.mp4 --name ichao --target ./example
+python preprocess.py --input_video ichao.mp4 --name ichao --target ../ViViD/data
