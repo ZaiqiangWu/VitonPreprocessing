f2279c874f1fa0a1de6575bc5c66c01ae11ab93c
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2025-05-26 18:14:01 +0900

    updated

--- run_fullbody.sh
+++ run_fullbody.sh
@@ -1,1 +1,3 @@
-python fullbody_preprocess.py --input_video raw_videos/jin_16_test.mp4 --name jin_16 --target ./data#../ViViD/data
+python fullbody_preprocess.py --input_video raw_videos/jin_16_test.mp4 --name jin_16_test --target ./data
+python fullbody_preprocess.py --input_video raw_videos/jin_16_train.mp4 --name jin_16_train --target ./data
+
